297cb2e0c4f4692c319095643928c37d5ab09f3d
ahmed ksontini <57683675+ahmedksont@users.noreply.github.com>
2026-01-05 14:16:51 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
-const repoName = "portfolio";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
 };
 
 export default nextConfig;
